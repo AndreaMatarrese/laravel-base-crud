a2b95a1fefe9c2946a028f7cd45a7b170dcf488f
AndreaMatarrese <matarreseandrea47@gmail.com>
2023-01-30 16:42:52 +0100

    mod

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,15 +2,17 @@
 
 @section('content')
 <h1>Comics</h1>
-    @foreach($comics as $comic)
-        <div class="card">
-            <h3>{{ $comic->title }}</h3>
-            <img src="{{ $comic->thumb }}" alt="img">
-            <div>{{ $comic->series }}</div>
-            <div>{{ $comic->description }}</div>
-            <div>{{ $comic->price }}</div>
-            <div>{{ $comic->sale_date }}</div>
-            <div>{{ $comic->type }}</div>
-        </div>
-    @endforeach
+    <div class="cards row row-cols-2">
+        @foreach($comics as $comic)
+            <div class="card">
+                <h3>{{ $comic->title }}</h3>
+                <img src="{{ $comic->thumb }}" alt="img">
+                <div>{{ $comic->series }}</div>
+                <div>{{ $comic->description }}</div>
+                <div>{{ $comic->price }}</div>
+                <div>{{ $comic->sale_date }}</div>
+                <div>{{ $comic->type }}</div>
+            </div>
+        @endforeach
+    </div>
 @endsection
